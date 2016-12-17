fb71d8dac4fdd5096a50b365db6ff2e7e59178d0
mchlltt <me@northwestern.edu>
2016-12-17 10:03:46 -0600

    Clean up for final submission

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 // Import node modules
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
 var fs = require('fs');
@@ -7,10 +7,19 @@ var inquirer = require('inquirer');
 var clear = require('clear');
 var figlet = require('figlet');
 var chalk = require('chalk');
+var twitterCredentials = true;
 
 // Import Twitter API secrets from keys.js
-var keys = require('./keys.js');
-var twitterKeys = keys.twitterKeys;
+try {var keys = require('./keys.js');}
+catch(err) {twitterCredentials = false;}
+
+// On run, clear the terminal then show the logo with figlet.
+clear();
+console.log(
+    chalk.cyan(
+        figlet.textSync('liri', {horizontalLayout: 'full'})
+    )
+);
 
 // Initial question.
 var mainQuestion = {
@@ -37,7 +46,7 @@ var mainQuestion = {
     ]
 };
 
-// Spotify search term.
+// Spotify search term questions.
 var spotifyFollowUp = [
     {
         type: 'input',
@@ -48,11 +57,13 @@ var spotifyFollowUp = [
         type: 'input',
         name: 'artist',
         message: 'Optional: What is the song\'s artist?',
-        when: function(answers) {return answers.song.length > 0}
+        when: function (answers) {
+            return answers.song.length > 0;
+        }
     }
 ];
 
-// OMDB search term.
+// OMDB search term question.
 var movieFollowUp = [
     {
         type: 'input',
@@ -61,8 +72,9 @@ var movieFollowUp = [
     }
 ];
 
-// Callable inquirer prompt for follow-up questions.
+// Inquirer prompt for follow-up questions.
 var followUpQuestion = function (type) {
+    console.log('');
     inquirer.prompt(type).then(
         function (answers) {
             switch (type[0].name) {
@@ -77,11 +89,10 @@ var followUpQuestion = function (type) {
                     break;
             }
         }
-    )
+    );
 };
 
 // Reads 'random.txt'
-// TODO: Make it actually do the contents of random.txt.
 var doWhatItSays = function () {
     fs.readFile('random.txt', 'utf8', function (err, data) {
         if (err) {
@@ -96,15 +107,8 @@ var doWhatItSays = function () {
     });
 };
 
-// Clear the terminal.
-clear();
-console.log(
-    chalk.cyan(
-        figlet.textSync('liri', { horizontalLayout: 'full' })
-    )
-);
-
-var processInitialAnswer = function(answers) {
+// Handle first answer.
+var processInitialAnswer = function (answers) {
     switch (answers.command) {
         case 'my-tweets':
             twitterCall();
@@ -133,16 +137,39 @@ var processInitialAnswer = function(answers) {
 };
 
 // Initial question inquirer prompt.
-inquirer.prompt(mainQuestion).then(processInitialAnswer);
+function mainPrompt() {
+    console.log('');
+    inquirer.prompt(mainQuestion).then(processInitialAnswer);
+}
 
+// Run initial prompt.
+mainPrompt();
+
+// Follow-up on loop.
+var repeatPrompt = function() {
+    console.log('');
+    inquirer.prompt({
+        name: 'repeat',
+        message: 'Is there anything else I can help you with?',
+        type: 'confirm'
+    }).then(function(answer) {
+       if (answer.repeat) {
+           mainPrompt();
+       } else {
+           console.log('\nHave a great day!\n');
+       }
+    });
+};
+
+// Call OMDB API with request package.
 var omdbCall = function (answers) {
     if (answers.movie.length === 0) {
         answers.movie = 'Mr. Nobody';
     }
     request('http://www.omdbapi.com/?t=' + answers.movie + '&y=&plot=short&r=json&tomatoes=true', function (error, response) {
         var body = JSON.parse(response.body);
-        if (!error && response.statusCode == 200 && body.Error === undefined) {
-            console.log(chalk.yellow('Results:'));
+        if (!error && response.statusCode === 200 && body.Error === undefined) {
+            console.log(chalk.yellow('\nResults:'));
             console.log('  Title: ' + body.Title);
             console.log('  Release Year: ' + body.Year);
             console.log('  IMDB Rating: ' + body.imdbRating);
@@ -152,11 +179,13 @@ var omdbCall = function (answers) {
             console.log('  Rotten Tomatoes Rating: ' + body.tomatoRating);
             console.log('  Rotten Tomatoes URL: ' + body.tomatoURL);
         } else {
-            console.log('Sorry, an error occurred. Please check your query and try again.');
+            console.log('\nSorry, an error occurred. Please check your query and try again.');
         }
-    })
+    });
+    setTimeout(repeatPrompt,1000);
 };
 
+// Call Spotify API with spotify package.
 var spotifyCall = function (answers) {
     if (answers.song.length === 0) {
         answers.song = 'The Sign';
@@ -170,18 +199,40 @@ var spotifyCall = function (answers) {
     }
 };
 
-var displaySpotifyResults = function(err, data) {
-    if (err) {
-        return console.log('Sorry, an error occurred. Please check your query and try again.');
+// Display the results from Spotify.
+var displaySpotifyResults = function (err, data) {
+    if (err || data.tracks.items.length === 0) {
+        console.log('\nSorry, an error occurred. Please check your query and try again.');
+    } else {
+        console.log(chalk.yellow('\nResults:'));
+        console.log('  Artist: ' + data.tracks.items[0].artists[0].name);
+        console.log('  Track Name: ' + data.tracks.items[0].name);
+        console.log('  Song preview: ' + data.tracks.items[0].preview_url);
+        console.log('  Album: ' + data.tracks.items[0].album.name);
     }
-    console.log(chalk.yellow('Results:'));
-    console.log('  Artist: ' + data.tracks.items[0].artists[0].name);
-    console.log('  Track Name: ' + data.tracks.items[0].name);
-    console.log('  Song preview: ' + data.tracks.items[0].preview_url);
-    console.log('  Album: ' + data.tracks.items[0].album.name);
+    setTimeout(repeatPrompt,1000);
 };
 
-
-var twitterCall = function (answers) {
-    console.log(answers);
+// Call Twitter API with twitter package.
+var twitterCall = function () {
+    if (twitterCredentials) {
+        var client = new Twitter(keys.twitterKeys);
+        var params = {screen_name: 'mchlltt'}; // Replace with your username!
+        client.get('statuses/user_timeline', params, function (error, tweets) {
+            if (error) {
+                return console.log('\nSorry, an error occurred. Please check your credentials and try again.');
+            }
+            var countTweets = tweets.length;
+            if (tweets.length > 20) {
+                countTweets = 20;
+            }
+            for (var i = countTweets - 1; i > 0; i--) {
+                console.log('\n' + tweets[i].created_at.slice(0,16));
+                console.log(tweets[i].text);
+            }
+        });
+    } else {
+        console.log('\nNo Twitter keys found!');
+    }
+    setTimeout(repeatPrompt,1000);
 };
